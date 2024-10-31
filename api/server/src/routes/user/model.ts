68ec0f4452177fd88e5a401ca8df3a16781e2ca1
dehypechief <dev.hype7@gmailcom>
2024-10-31 07:46:54 +0100

    done

--- api/server/src/routes/user/model.ts
+++ api/server/src/routes/user/model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document, ObjectId, Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; 
 import validator from 'validator';
 
 interface IUser extends Document {
