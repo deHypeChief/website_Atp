deefebe231dcfc7f910f01445d6b4a9497c42215
dehypechief <dev.hype7@gmailcom>
2024-12-10 09:20:07 +0100

    update team

--- client/src/routes/about.jsx
+++ client/src/routes/about.jsx
@@ -5,28 +5,31 @@ import "../libs/styles/about.css"
 
 import heroImg from "../libs/images/main/IMG_2800.jpg"
 import heroImg2 from "../libs/images/main/IMG_2809.jpg"
+import { useEffect, useState } from "react";
+import { client } from "../sanityClient";
 
 export default function About() {
-	const [about, setAbout] = useState([]);
+    const [about, setAbout] = useState([]);
     const [loading, setLoading] = useState(true); // Loading state
-	const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-		async function getReviews() {
-			setLoading(true); // Set loading state to true
-			try {
-				const data = await client.fetch(`
+        async function getAbout() {
+            setLoading(true); // Set loading state to true
+            try {
+                const data = await client.fetch(`
 					*[_type == "atpContent"]
 				`);
-				setAbout(data);
-			} catch (err) {
-				setError("Failed to load reviews");
-			} finally {
-				setLoading(false); // Set loading state to false after fetching is done
-			}
-		}
-		getReviews();
-	}, []);
+                setAbout(data[0]);
+                console.log(data[0])
+            } catch (err) {
+                setError("Failed to load reviews");
+            } finally {
+                setLoading(false); // Set loading state to false after fetching is done
+            }
+        }
+        getAbout();
+    }, []);
     return (
         <>
             <Hero title={"About us"} subTitle={"Who and what we are"} imageUrl={heroImg} noAction />
@@ -47,12 +50,12 @@ export default function About() {
                     <div className="storyText">
                         <h1>Our Story</h1>
                         <p>
-                        At ATP, we believe tennis is more than just a sport; it’s a lifestyle and a lifelong pursuit of mastery. That’s why we offer high-quality training tailored to each player’s unique level and goals. Whether you’re a complete beginner discovering the thrill of tennis for the first time, or an experienced player looking to polish your technique, ATP provides the guidance, resources, and community support to help you reach new heights.
+                            At ATP, we believe tennis is more than just a sport; it’s a lifestyle and a lifelong pursuit of mastery. That’s why we offer high-quality training tailored to each player’s unique level and goals. Whether you’re a complete beginner discovering the thrill of tennis for the first time, or an experienced player looking to polish your technique, ATP provides the guidance, resources, and community support to help you reach new heights.
 
                         </p>
                         <br /><br />
                         <p>
-                        What makes ATP special is our inclusive, welcoming community. Here, anyone—male or female, young or seasoned—can experience the joy of tennis. We are proud to serve players at our premium training locations, including Transcorp Hilton, Rockview Hotels (Royale), and the Moshood Abiola National Stadium, providing state-of-the-art facilities for training and recreation.
+                            What makes ATP special is our inclusive, welcoming community. Here, anyone—male or female, young or seasoned—can experience the joy of tennis. We are proud to serve players at our premium training locations, including Transcorp Hilton, Rockview Hotels (Royale), and the Moshood Abiola National Stadium, providing state-of-the-art facilities for training and recreation.
 
                         </p>
                     </div>
@@ -65,11 +68,16 @@ export default function About() {
             <section className="aims">
                 <div className="aimfBox">
                     <h1>Our Vision</h1>
-                    {about?.aboutVisionText || ""}
+                    <p>
+                        {about?.aboutVisionText || ""}
+
+                    </p>
                 </div>
                 <div className="aimfBox">
                     <h1>Our Mission</h1>
-                    {about?.aboutMissionText || ""}
+                    <p>
+                        {about?.aboutMissionText || ""}
+                    </p>
                 </div>
             </section>
 
@@ -77,7 +85,10 @@ export default function About() {
             <section className="teams">
                 <div className="teamText">
                     <h1>Our Team</h1>
-                    {about?.teamText || ""}
+                    <p>
+                        {about?.teamText || ""}
+
+                    </p>
                 </div>
 
                 <div className="teamCards">
@@ -92,7 +103,7 @@ export default function About() {
                 imageUrl={heroImg2}
                 altText={"Contact Us"}
                 altLink={"/contact"}
-                text={"Cras tincidunt ligula ac enim posuere venenatis. In luctus biben dum nisl, in luctus dolor ultrices volutpat."}
+            // text={"Cras tincidunt ligula ac enim posuere venenatis. In luctus biben dum nisl, in luctus dolor ultrices volutpat."}
             />
 
         </>
